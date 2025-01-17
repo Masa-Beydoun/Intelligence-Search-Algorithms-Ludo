ecee33b3f25d32ef3432c99ec5f807a1aa0781f4
Depsidee <deedepsi@gmail.com>
2025-01-17 23:54:47 +0300

    Turn has been edited.

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -4,14 +4,14 @@ public class Algorithm {
 
     private static final int MAX_DEPTH = 3;
 
-    public State bestState(State root, int turn) {
+    public State bestState(State root) {
 
         double bestValue = Double.NEGATIVE_INFINITY;
         State bestState = null;
 
-        List<State> nextStates = root.nextStates(turn);
+        List<State> nextStates = root.nextStates(root.turn);
         for (State state : nextStates) {
-            double value = expectimax(state, MAX_DEPTH, turn);
+            double value = expectimax(state, 0, root.turn);
             if (value > bestValue) {
                 bestValue = value;
                 bestState = state;
@@ -23,19 +23,17 @@ public class Algorithm {
 
     private double expectimax(State state, int depth, int turn) {
 
-        if (state.checkGoal()) {
+        if (depth == MAX_DEPTH || state.checkGoal()) {
             return state.heuristic();
         }
 
-        int nextTurn = turn = (turn + 1) % 4;
-
-        if (turn %2 == 0) {
+        if (turn == 0) {
 
             double maximizer = Double.NEGATIVE_INFINITY;
 
             List<State> nextStates = state.nextStates(turn);
             for (State nextState : nextStates) {
-                double max = expectimax(nextState, depth + 1, nextTurn);
+                double max = expectimax(nextState, depth + 1, nextState.turn);
                 maximizer = Math.max(maximizer, max);
             }
 
@@ -48,7 +46,7 @@ public class Algorithm {
             List<State> nextStates = state.nextStates(turn);
             double probability = 1.0 / 6;
             for (State nextState : nextStates) {
-                chance += probability * expectimax(nextState, depth - 1, nextTurn);
+                chance += probability * expectimax(nextState, depth - 1, nextState.turn);
             }
 
             return chance;
